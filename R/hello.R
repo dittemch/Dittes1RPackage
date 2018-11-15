357d90330e6fcae23c51271fd744fef845c14014
Ditte Marie Christiansen <dich9390@win.su.se>
2018-11-15 10:47:17 +0100

    hejhej

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,29 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' Hello. This package is first try to make a package
+#'
+#' The function returns a sumtable and made for the dataset dafnia
+#'
+#' @param y is the response variable as col in dataframe
+#' @param x1 is factor that needs to group the response together with
+#' @param x2 is the same
+#'
+#' @return Gives sumtable of dafnia grouped by lake and temperature
+#' @examples sumtable(dafnia$Feeding,dafnia$Temp,dafnia$Lake) - if you put in a default then write it here
+#' @export
 
 hello <- function() {
   print("Hello, world!")
 }
+
+
+sumtable<-function(y,x1,x2){
+  SumTable <- aggregate(y,
+                        by = list(Lake = x1,
+                                  Temp = x2),
+                        FUN = function(x)
+                          c(mean = mean(x),
+                            sd = sd(x),
+                            n = length(x)))
+  return(SumTable)
+}
+
+
